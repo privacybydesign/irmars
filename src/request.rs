a0f515b6f880fcecea27137b6d2fd4e0ad9c7b4c
Leon <leon.botros@ru.nl>
2020-09-22 15:34:16 +0200

    feat: ordering for AttributeRequests

--- src/request.rs
+++ src/request.rs
@@ -1,19 +1,41 @@
 use serde::ser::SerializeStruct;
 use serde::{Deserialize, Serialize, Serializer};
+use std::cmp::Ordering;
 use std::collections::BTreeMap;
+
+/// An IRMA AttributeType identifies an attribute
+#[derive(Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct AttributeType {
+    pub scheme: String,
+    pub issuer: String,
+    pub credential: String,
+    pub attribute: String,
+}
+
+/// An instance of an IRMA attribute, a type and optionally a value
+#[derive(Serialize, Deserialize, PartialEq, Eq, PartialOrd)]
+pub struct Attribute {
+    #[serde(rename = "type")]
+    pub atype: AttributeType,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub value: Option<String>,
+}
+
+impl Ord for Attribute {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.atype.cmp(&other.atype)
+    }
+}
 
 /// An AttributeRequest asks for an instance of an attribute type,
 /// possibly requiring it to have a specified value, in a session request.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Eq, PartialOrd, PartialEq, Ord)]
 pub struct AttributeRequest {
-    #[serde(rename = "type")]
-    pub atype: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub value: Option<String>,
+    pub attribute: Attribute,
     pub not_null: bool,
 }
 
-/// A conjuction of attribute requests, only satisfied
+/// A conjunction of attribute requests, only satisfied
 /// when all of its containing attribute requests are satisfied.
 #[derive(Serialize, Deserialize)]
 pub struct AttributeCon(pub Vec<AttributeRequest>);
@@ -48,5 +70,45 @@ impl Serialize for DisclosureRequest {
         }
 
         dr.end()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sort() {
+        let req = AttributeCon {
+            0: vec![
+                AttributeRequest {
+                    attribute: Attribute {
+                        atype: AttributeType {
+                            scheme: String::from("pbdf"),
+                            issuer: String::from("pbdf"),
+                            credential: String::from("email"),
+                            attribute: String::from("email"),
+                        },
+                        value: None,
+                    },
+                    not_null: true,
+                },
+                AttributeRequest {
+                    attribute: Attribute {
+                        atype: AttributeType {
+                            scheme: String::from("pbdf"),
+                            issuer: String::from("pbdf"),
+                            credential: String::from("email"),
+                            attribute: String::from("domain"),
+                        },
+                        value: None,
+                    },
+                    not_null: true,
+                },
+            ],
+        };
+
+        // Because 'e' comes after 'd'
+        assert_eq!(Ordering::Greater, req.0[0].cmp(&req.0[1]));
     }
 }
